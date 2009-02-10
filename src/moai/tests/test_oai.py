9a481968c8bc1f818fbc8ecd3c8fab2822054471
jasper@882ca7fe-3cf8-0310-95c5-f9aabef819dd <none@none>
2009-02-10 11:50:46 +0000

    Added SQLite tests
    --HG--
    extra : convert_revision : svn%3A882ca7fe-3cf8-0310-95c5-f9aabef819dd/MOAI/trunk%4033412

--- src/moai/tests/test_oai.py
+++ src/moai/tests/test_oai.py
@@ -6,14 +6,21 @@ from moai.provider.list import ListBasedContentProvider
 from moai.content import DictBasedContentObject
 from moai.update import DatabaseUpdater
 from moai.database.btree import BTreeDatabase
+from moai.database.sqlite import SQLiteDatabase
 from moai.server import Server, FeedConfig
 from moai.http.cgi import CGIRequest
 from moai.tests.test_database import DATA
+from moai.core import MOAI
 
-class OAITest(TestCase):
+class OAIBtreeTest(TestCase):
+
+    def get_database(self):
+        return BTreeDatabase()
 
     def setUp(self):
-        self.db = BTreeDatabase()
+        # init moai, so metadata formats get registered
+        m = MOAI(logging, debug=True)
+        self.db = self.get_database()
         provider = ListBasedContentProvider(DATA)
         updater = DatabaseUpdater(provider, DictBasedContentObject, self.db, logging)
         updater.update_provider()
@@ -26,7 +33,7 @@ class OAITest(TestCase):
 
 
     def request(self, **kwargs):
-        req = CGIRequest('http://localhost/repo/test', **kwargs)
+        req = CGIRequest(u'http://localhost/repo/test', **kwargs)
         req.stream = StringIO.StringIO()
         self.server.handle_request(req)
         req.stream.seek(0)
@@ -37,39 +44,62 @@ class OAITest(TestCase):
         del self.server
 
     def test_identify(self):
-        response = self.request(verb='Identify')
+        response = self.request(verb=u'Identify')
         self.assertEquals('Status: 200 OK', response.splitlines()[0])
         self.assertTrue(
             '<repositoryName>A test Repository</repositoryName>' in response)
 
     def test_list_identifiers(self):
-        response = self.request(verb='ListIdentifiers', metadataPrefix='oai_dc')
+        response = self.request(verb=u'ListIdentifiers', metadataPrefix=u'oai_dc')
         self.assertEquals(response.count('<identifier>'), 3)
 
     def test_list_records(self):
-        response = self.request(verb='ListRecords', metadataPrefix='oai_dc')
+        response = self.request(verb=u'ListRecords', metadataPrefix=u'oai_dc')
         self.assertEquals(response.count('<identifier>'), 3)
         self.assertEquals(response.count('<metadata>'), 3)
 
     def test_get_record(self):
-        response = self.request(verb='GetRecord', identifier='oai:id:1',
-                                metadataPrefix='oai_dc')
+        response = self.request(verb=u'GetRecord', identifier=u'oai:id:1',
+                                metadataPrefix=u'oai_dc')
         self.assertEquals(response.count('<identifier>'), 1)
         self.assertTrue('<identifier>oai:id:1</identifier>' in response)
-        response = self.request(verb='GetRecord', identifier='oai:id:2',
-                                metadataPrefix='oai_dc')
-        self.assertEquals(response.count('<identifier>'), 1)
-        self.assertTrue('<identifier>oai:id:2</identifier>' in response)
-        response = self.request(verb='GetRecord', identifier='oai:id:3',
-                                metadataPrefix='oai_dc')
+        
+        response = self.request(verb=u'GetRecord', identifier=u'oai:id:3',
+                                metadataPrefix=u'oai_dc')
         self.assertEquals(response.count('<identifier>'), 1)
         self.assertTrue('<identifier>oai:id:3</identifier>' in response)
-
         
+        response = self.request(verb=u'GetRecord', identifier=u'oai:id:2',
+                                metadataPrefix=u'oai_dc')
+        self.assertEquals(response.count('<identifier>'), 1)
+        self.assertTrue('<identifier>oai:id:2</identifier>' in response)
+
+    def test_disallowed_record(self):
+        config = FeedConfig('test', 'A test Repository',
+                            'http://localhost/repo/test',
+                            logging,
+                            sets_disallowed=[u'datasets'])
+        self.server = Server('http://localhost/repo', self.db)
+        self.server.add_config(config)
+        # identifier 2 should not be in the identifier list
+        response = self.request(verb=u'ListIdentifiers', metadataPrefix=u'oai_dc')
+        self.assertFalse('<identifier>oai:id:2</identifier>' in response)
+        response = self.request(verb=u'GetRecord', identifier=u'oai:id:2',
+                                metadataPrefix=u'oai_dc')
+        self.assertTrue('<error code="idDoesNotExist">oai:id:2</error>' in response)
+
+
+class OAISQLiteTest(OAIBtreeTest):
+    
+    def get_database(self):
+        return SQLiteDatabase()
+
    
 def test_suite():
-    return TestSuite((makeSuite(OAITest), ))
-
+    suite = TestSuite()
+    suite.addTest(makeSuite(OAIBtreeTest))
+    suite.addTest(makeSuite(OAISQLiteTest))
+    return suite
 
 if __name__ == '__main__':
     main(defaultTest='test_suite')
