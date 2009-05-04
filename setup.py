44168c88626181cbc8871cd035d635513e2f223a
jasper@882ca7fe-3cf8-0310-95c5-f9aabef819dd <none@none>
2009-05-04 15:56:11 +0000

    bumped version after release
    --HG--
    extra : convert_revision : svn%3A882ca7fe-3cf8-0310-95c5-f9aabef819dd/MOAI/trunk%4034952

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='MOAI',
-    version='1.0',
+    version='1.1dev',
     author='Infrae',
     author_email='jasper@infrae.com',
     description="MOAI, A Open Access Server Platform for Institutional Repositories",
