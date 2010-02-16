7382d64e0bd82c86d13d5ca628be473832d0eefe
jasper@882ca7fe-3cf8-0310-95c5-f9aabef819dd <none@none>
2010-02-16 10:57:56 +0000

    tag for release
    --HG--
    extra : convert_revision : svn%3A882ca7fe-3cf8-0310-95c5-f9aabef819dd/MOAI/trunk%4039856

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='MOAI',
-    version='1.0.8',
+    version='1.0.9',
     author='Infrae',
     author_email='jasper@infrae.com',
     url='http://moai.infrae.com',
