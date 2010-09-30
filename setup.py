e28c76a390fa22aae923327f67c7e71999f9e388
"Jasper Op de Coul ext:(%22) <jasper@infrae.com>
2010-09-30 11:01:08 +0200

    Back to development: 1.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='MOAI',
-    version='1.1.2',
+    version='1.1.3dev',
     author='Infrae',
     author_email='info@infrae.com',
     url='http://moai.infrae.com',
