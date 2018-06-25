3c54c6966bb022d29c4ca3a2a0b3a24fd090a0e7
Felix A. Croes <felix.croes@profit4cloud.nl>
2018-06-25 11:32:19 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='MOAI',
-    version='2.2.7',
+    version='2.2.8',
     author='Infrae',
     author_email='info@infrae.com',
     url='http://infrae.com/products/moai',
