48b0465d8fdc90c07fd7b26f37a29e270d138dcc
Sietse Snel <s.t.snel@uu.nl>
2023-02-17 12:45:07 +0100

    Re-add MODS, DIDL formats
    Needed for running the upstream integration tests. They can
    be disabled in the settings.ini file by default.

--- setup.py
+++ setup.py
@@ -48,7 +48,9 @@ setup(
         'oai_dc=moai.metadata.oaidc:OAIDC',
         'datacite=moai.metadata.datacite:DataCite',
         'oai_datacite=moai.metadata.datacite:DataCite',
-        'iso19139=moai.metadata.iso:Iso'
+        'iso19139=moai.metadata.iso:Iso',
+        'mods=moai.metadata.mods:MODS',
+        'didl=moai.metadata.didl:DIDL'
      ],
     },
     install_requires=[
