9f75283d90e299149be3adc0fea717d68c56e8c0
Harm de Raaff <harm.apple@gmail.com>
2023-04-03 10:36:20 +0200

    fixed errors

--- moai/metadata/iso.py
+++ moai/metadata/iso.py
@@ -189,10 +189,10 @@ class Iso(object):
                     affiliation_list = [affiliation_list]
 
                 for affiliation in affiliation_list:
-                    if alliation is dict:
+                    if affiliation is dict:
                         CI_ResponsibleParty.append(GMD.organisationName(GCO.CharacterString(affiliation["Affiliation_Name"])))
                     else:
-                        CI_ResponsibleParty.append(GMD.organisationName(GCO.CharacterString(affiliation))
+                        CI_ResponsibleParty.append(GMD.organisationName(GCO.CharacterString(affiliation)))
 
                 CI_ResponsibleParty.append(role)
 
